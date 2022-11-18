c1b550dbe61cb525c0d8207359d8427be0b022a6
nxnil105 <113341774+nxnil105@users.noreply.github.com>
2022-11-18 17:08:14 +0600

    Create nox.py

---
+++ nox.py
@@ -0,0 +1,19 @@
+import os,re,sys,platform
+ 
+os.system('git pull')
+ 
+os.system('pkg install play-audio')
+ 
+import requests
+ 
+ 
+ 
+bit = platform.architecture()[0]
+ 
+if bit == '64bit':
+if bit == '32bit':
+ 
+    from nox import nox
+ 
+    Main()
+    
